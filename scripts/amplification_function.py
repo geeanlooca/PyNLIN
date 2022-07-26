48e4bf4216e3990dadd2be995a8a3a1f2a68ea2f
lorenzifrancesco <francesco.lorenzi98@gmail.com>
2022-07-26 11:57:30 +0200

    sim: co profiles, 70km

--- scripts/amplification_function.py
+++ scripts/amplification_function.py
@@ -37,7 +37,7 @@ parser.add_argument(
 parser.add_argument(
     "-L",
     "--fiber-length",
-    default=80,
+    default=70,
     type=float,
     help="The length of the fiber in kilometers.",
 )
