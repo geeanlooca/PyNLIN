86e144d2edf87589c0046dbde81fc8ed4b94f723
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2022-07-26 14:02:17 +0200

    sim: Gaussian [0, 9]

--- scripts/time_integrals.py
+++ scripts/time_integrals.py
@@ -141,7 +141,7 @@ m = pynlin.nlin.get_m_values(fiber, fiber_length, channel_spacing, 1 / baud_rate
 # partial_collision_margin = 5
 # points_per_collision = 10
 
-interfering_index = [19, 29]
+interfering_index = [0, 9]
 
 pynlin.nlin.X0mm_time_integral_WDM_selection(
     baud_rate,
@@ -150,7 +150,7 @@ pynlin.nlin.X0mm_time_integral_WDM_selection(
     fiber,
     fiber_length,
     "0_9_results.h5",
-    pulse_shape="Nyquist",
+    pulse_shape="Gaussian",
     rolloff_factor=0.1,
     samples_per_symbol=10,
     points_per_collision=points_per_collision,
