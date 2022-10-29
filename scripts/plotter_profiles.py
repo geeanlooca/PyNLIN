a8a4d130581d79aa77fd7ee77809784ffb92ee51
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2022-10-29 15:47:40 +0200

    fix: typo

--- scripts/plotter_profiles.py
+++ scripts/plotter_profiles.py
@@ -56,9 +56,9 @@ for fiber_length in fiber_lengths:
     results_path_ct = '../results_' + str(length_setup) + '/' + str(num_only_ct_pumps) + '_ct/'
     results_path_bi = '../results_' + str(length_setup) + '/' + str(num_co) + '_co_' + str(num_ct) + '_ct_' + special + '/'
     #
-    optimization_result_path_co = '../results_' + str(length_setup) + '/optimization/' + str(num_only_co_pumps) + '_co/'
-    optimization_result_path_ct = '../results_' + str(length_setup) + '/optimization/' + str(num_only_ct_pumps) + '_ct/'
-    optimization_result_path_bi = '../results_' + str(length_setup) + '/optimization/' + str(num_co) + '_co_' + str(num_ct) + '_ct_' + special + '/'
+    optimized_result_path_co = '../results_' + str(length_setup) + '/optimization/' + str(num_only_co_pumps) + '_co/'
+    optimized_result_path_ct = '../results_' + str(length_setup) + '/optimization/' + str(num_only_ct_pumps) + '_ct/'
+    optimized_result_path_bi = '../results_' + str(length_setup) + '/optimization/' + str(num_co) + '_co_' + str(num_ct) + '_ct_' + special + '/'
     time_integrals_results_path = '../results/'
 
     # PLOTTING PARAMETERS
