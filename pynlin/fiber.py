fix: units of the fitting procedure

pynlin/fiber.py
@@ -106,8 +106,8 @@ class MMFiber:
           # default super-approximated case: all the modes overlap as the fundamental one.
           # No cross-overlap
           if overlap_integrals_avg is None: 
-            # self.overlap_integrals_avg = 1/effective_area * np.identity(modes)   
-            self.overlap_integrals_avg = np.array([[1. , 2.], [3. , 4.]])       
+            self.overlap_integrals_avg = 1/effective_area * np.identity(modes)   
+            # self.overlap_integrals_avg = np.array([[1. , 2.], [3. , 4.]])       
             self.overlap_integrals = self.overlap_integrals_avg[None, :, :].repeat(6, axis=0)
             
             for i in range(5):
@@ -118,19 +118,14 @@ class MMFiber:
           self.overlap_integrals = self.overlap_integrals_avg[None, :, :].repeat(6, axis=0)
           for i in range(5):
             self.overlap_integrals[i, :, :] *= 0.0
+          self.overlap_integrals_avg = self.overlap_integrals_avg[:modes, :modes]
         else:
           self.overlap_integrals = overlap_integrals
-          self.overlap_integrals = self.overlap_integrals[:, :modes, :modes]
-          self.overlap_integrals_avg = self.overlap_integrals[0, :, :]
-          
-        # adjust for mismatches of OI matrix and selected mode number 
-        # print(np.shape(self.overlap_integrals))
-        # print(np.shape(self.overlap_integrals_avg))  
-        self.overlap_integrals_avg = self.overlap_integrals_avg[:modes, :modes]
-        self.overlap_integrals = self.overlap_integrals[:, :modes, :modes]
+          self.overlap_integrals_avg = self.overlap_integrals[-1, :, :]
+          # adjust for mismatches of OI matrix and selected mode number 
         
-        print("==========================")
-        print(self.overlap_integrals[-1, :, :])
+        self.overlap_integrals = self.overlap_integrals[:, :modes, :modes]
+        print(self.overlap_integrals)
         
         self.mode_names = mode_names
 

pynlin/raman/solvers.py
@@ -356,10 +356,10 @@ class RamanAmplifier:
             -z[:, np.newaxis] * signal_losses
         )
 
-        plt.figure()
-        plt.plot(z, pynlin.utils.watt2dBm(signal_solution_initial_cond))
-        plt.plot(z, pynlin.utils.watt2dBm(pump_solution_initial_cond))
-        plt.show()
+        # plt.figure()
+        # plt.plot(z, pynlin.utils.watt2dBm(signal_solution_initial_cond))
+        # plt.plot(z, pynlin.utils.watt2dBm(pump_solution_initial_cond))
+        # # plt.show()
 
         initial_conditions = np.hstack(
             (pump_solution_initial_cond, signal_solution_initial_cond)
@@ -631,12 +631,10 @@ class MMFRamanAmplifier(RamanAmplifier):
             https://www.osapublishing.org/abstract.cfm?uri=OFC-2012-OW1D.2
             (June 5, 2019).
         """
-
         num_signals = signal_power.shape[0]
         num_pumps = pump_power.shape[0]
 
         total_wavelengths = num_signals + num_pumps
-        print(num_pumps)
         num_modes = fiber.modes
         total_signals = total_wavelengths
         pump_power_ = pump_power.reshape((num_modes * num_pumps))
@@ -680,7 +678,10 @@ class MMFRamanAmplifier(RamanAmplifier):
         mode_list = np.array(range(fiber.modes))
         # change the order of creation
         oi = fiber.get_oi_matrix(mode_list, wavelengths)
-
+        plt.figure(figsize=(10, 8))
+        sns.heatmap(oi, annot=True, cmap='viridis', linewidths=.5, fmt='.2e')
+        # plt.show()
+        
         gain_matrix = freq_scaling * gains
 
         gain_matrix = gain_matrix.repeat(fiber.modes, axis=0).repeat(

pynlin/utils.py
@@ -66,12 +66,11 @@ def dispersion_to_beta2(D, wavelength):
 
 def oi_law(l1, l2, params):
   a1, b1, a2, b2, x, c = params
-  return (a1 * np.square(l1) + b1 * l1 + a2 * np.square(l2) + b2 * l2 + x * l1 * l2 + c).ravel()
+  return (a1 * np.square(l1) + b1 * l1 + a2 * np.square(l2) + b2 * l2 + x * l1 * l2 + c)
 
 def oi_law_fit(L, a1, b1, a2, b2, x, c):
   l1, l2 = L
   return (a1 * np.square(l1) + b1 * l1 + a2 * np.square(l2) + b2 * l2 + x * l1 * l2 + c).ravel()
-
 class OpticalBands(Enum):
     """Class enumerating the optical transmission bandwidths.
 

scripts/MMF_profile_example.py
@@ -11,7 +11,10 @@ import pynlin.nlin
 import pynlin.utils
 import pynlin.fiber
 import matplotlib.pyplot as plt
+from matplotlib.cm import inferno
 from matplotlib.cm import viridis
+from pynlin.utils import oi_law, oi_law_fit
+cmap = inferno
 
 plt.rcParams.update({
     "text.usetex": False,
@@ -44,7 +47,7 @@ def get_final_signals(
     oi_fit = np.load('oi_fit.npy')
     oi_max = np.load('oi_max.npy')
     oi_min = np.load('oi_min.npy')
-
+    
     beta2 = pynlin.utils.dispersion_to_beta2(
         dispersion, wavelength
     )
@@ -55,7 +58,6 @@ def get_final_signals(
           oi_avg = oi_max
       else:
           oi_avg = oi_min
-      oi_avg[0, 3] *= 10
       oi_fit = None
     else:
       oi_avg = None
@@ -68,7 +70,6 @@ def get_final_signals(
         overlap_integrals_avg=oi_avg
     )
 
-    
     wdm = pynlin.wdm.WDM(
         spacing=channel_spacing,
         num_channels=num_channels,
@@ -81,40 +82,21 @@ def get_final_signals(
     channel_spacing = interfering_frequency - frequency_of_interest
 
     fiber_length = fiber_lengths[0]
-    print("Warning: selecting only the first fiber length")
-    print("Warning: selecting only the first gain")
+    print("Warning: selecting only the first fiber length and gain")
 
     # Waves parameters ===========================
-
-    # --- uniform pump frequencies
-    # pump_band_b = lambda2nu(1480e-9)
-    # pump_band_a = lambda2nu(1400e-9)
-    # initial_pump_frequencies = np.linspace(pump_band_a, pump_band_b, num_pumps)
-
-    # --- BROMAGE pump frequencies
-    # initial_pump_frequencies = np.array(
-        # lambda2nu([1647e-9, 1467e-9, 1485e-9, 1515e-9]))
-
-
     # --- JLT-NLIN-1
     initial_pump_frequencies = np.array(
         lambda2nu([1429e-9, 1465e-9, 1488e-9, 1514e-9]))
     # initial_pump_frequencies = np.array(
         # lambda2nu([1449e-9]))
     
-    # --- uniform power
-    # power_per_pump = dBm2watt(-10)
-    # pump_powers = np.ones((len(initial_pump_frequencies), num_modes)) * power_per_pump
-    
     # --- JLT-NLIN-1 power
     # warn: these are the powers at the fiber end
-    pump_powers = dBm2watt(np.array([19.6-32, 17.3-32, 19.6-32, 14.5-32]))
-    num_pumps = 1
+    pump_powers = dBm2watt(np.array([19.6-22, 17.3-22, 19.6-22, 14.5-22]))
+    num_pumps = 4
     initial_pump_frequencies = initial_pump_frequencies[:num_pumps]
     pump_powers = pump_powers[:num_pumps][:, None].repeat(num_modes, axis=1)
-    pump_powers[:, 1:] *= 0
-    print(pump_powers)
-    # pump_powers[2, 2] *= 0.0001
     power_per_channel = dBm2watt(-30)
 
     # PRECISION REQUIREMENTS ESTIMATION =================================
@@ -147,15 +129,14 @@ def get_final_signals(
     )
     # fixed mode
     plt.clf()
-    cmap = viridis
     z_plot = np.linspace(0, fiber_length, len(pump_solution[:, 0, 0]))[:, None] * 1e-3
     for i in range(num_pumps):
         if i == 1:
             plt.plot(z_plot,
-                     watt2dBm(pump_solution[:, i, :]), label="pump", color=cmap(i / num_pumps), ls="--")
+                     watt2dBm(pump_solution[:, i, :]), label="pump", color=cmap(i / num_pumps), ls="--", lw=0.5)
         else:
             plt.plot(z_plot,
-                     watt2dBm(pump_solution[:, i, :]), color=cmap(i / num_pumps), ls="--")
+                     watt2dBm(pump_solution[:, i, :]), color=cmap(i / num_pumps), ls="--", lw=0.5)
     for i in range(num_channels):
         if i == 1:
             plt.plot(z_plot, watt2dBm(signal_solution[:, i, :]), color=cmap(
@@ -172,18 +153,33 @@ def get_final_signals(
 
 max_min = []
 max_min.append(get_final_signals(max_oi=True , use_average=True))
-# max_min.append(get_final_signals(max_oi=False, use_average=True))
-# max_min.append(get_final_signals(max_oi=False, use_average=False))
+max_min.append(get_final_signals(max_oi=False, use_average=True))
+max_min.append(get_final_signals(max_oi=False, use_average=False))
 num_channels = len(max_min[0][:, 0])
 num_modes = len(max_min[0][0, :])
-cmap = viridis
+plt.clf()
 for imod in range(num_modes):
     plt.plot(range(num_channels), watt2dBm(
-        max_min[0][:, imod]), color=cmap(imod / num_channels), label="max" if imod==1 else None, lw=0.8)
-    # plt.plot(range(num_channels), watt2dBm(
-    #     max_min[1][:, imod]), color=cmap(imod / num_channels), ls="--", label="min" if imod==1 else None, lw=0.8)
-    # plt.plot(range(num_channels), watt2dBm(
-    #     max_min[2][:, imod]), color=cmap(imod / num_channels), ls=":", label="fit" if imod==1 else None, lw=0.8)
+        max_min[0][:, imod]), color=cmap(imod / num_modes), label="max", lw=0.8)
+    plt.plot(range(num_channels), watt2dBm(
+        max_min[1][:, imod]), color=cmap(imod / num_modes), ls="--", label="min", lw=0.8)
+    plt.plot(range(num_channels), watt2dBm(
+        max_min[2][:, imod]), color=cmap(imod / num_modes), ls=":", label="fit", lw=0.8)
 plt.legend()
 plt.grid()
-plt.savefig("media/spectra_comparison.pdf")
+plt.ylabel("Output power [dBm]")
+plt.xlabel("Channel number")
+plt.savefig("media/LP01.pdf")
+    # plt.plot(range(num_channels), watt2dBm(
+    #     max_min[2][:, imod]), color=cmap(imod / num_channels), ls=":", label="fit" if imod==1 else None, lw=0.8)
+for i in range(3):
+  plt.clf()
+  plt.fill_between(range(num_channels), np.max(watt2dBm(
+        max_min[i][:, :]), axis=1), np.min(watt2dBm(
+        max_min[i][:, :]), axis=1), color=cmap(i / 3), ls="--", label=["max", "min", "fit"][i], lw=0.8, alpha=0.2)
+  
+  plt.legend()
+  plt.grid()
+  plt.ylabel("Output power [dBm]")
+  plt.xlabel("Channel number")
+  plt.savefig("media/MGD"+str(i)+".pdf")

scripts/oi_fit.py
@@ -4,29 +4,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
 import plotly.graph_objects as go
-from pynlin.utils import oi_law_fit
-
+from pynlin.utils import oi_law_fit, oi_law
+from matplotlib.cm import viridis
 rc('text', usetex=False)
 
 oi_file = 'oi.mat'
 mat = scipy.io.loadmat(oi_file)
 oi_full = mat['OI'] * 1e12
-wl = mat['wavelenght_array'][0] * 1e-3
+wl = mat['wavelenght_array'][0] * 1e-9
 # average over the polarizations
 oi = np.ndarray((21, 21, 4, 4))
 oi_avg = np.ndarray((4, 4))
 oi_max = np.zeros_like(oi_avg)
 oi_min = np.zeros_like(oi_avg)
-# print(wl)
 
 # return starting and ending index of the polarization
 
+"""
+WARN: probably wrong
+"""
+def expression(l1i, l1f, params):
+  l2i=l1i
+  l2f=l1f
+  Lambda = (l1f-l1i)
+  a1, a2, b1, b2, c, x = params
+  integral = Lambda * ((a1+a2)*(l1f**3-l1i**3)/3 + (b1+b2) * (l1f**2-l1i**2)/2) + x * (l1f**2-l1i**2)*(l2f**2-l2i**2)/4 + c * Lambda**2
+  return integral / Lambda**2
 
 def polix(f):
     ll = [2, 4, 4, 2]
     st = [0, 2, 6, 10]
     return (st[f], st[f] + ll[f])
-
 
 for i in range(4):
     for j in range(4):
@@ -35,12 +43,11 @@ for i in range(4):
         oi_avg[i, j] = np.mean(oi[:, :, i, j])
         oi_max[i, j] = np.max(oi[:, :, i, j])
         oi_min[i, j] = np.min(oi[:, :, i, j])
-        
+
 np.save('oi.npy', oi)
 np.save('oi_avg.npy', oi_avg)
 np.save('oi_max.npy', oi_max)
 np.save('oi_min.npy', oi_min)
-
 
 # quadratic fit of the OI in frequency
 oi_fit = np.ndarray((6, 4, 4))
@@ -49,15 +56,25 @@ x, y = np.meshgrid(wl, wl)
 for i in range(4):
     for j in range(4):
         oi_fit[:, i, j] = curve_fit(
-            oi_law_fit, (x, y), oi[:, :, i, j].ravel(), p0=[0, 1, 0, 1, 0, 1])[0].T
+            oi_law_fit, (x, y), oi[:, :, i, j].ravel(), p0=[1e10, 1e10, 1e10, 1e10, 0, 1])[0].T
 # print(np.shape(oi_fit))
 np.save('oi_fit.npy', oi_fit)
 
-print(oi_fit[-1, :, :])
-# plt.plot(oi_fit[:, 1, 1])
-print(oi_max[:, :])
-print(oi_min[:, :])
 
+for mode in range(1):
+  color = viridis(mode/4)
+  plt.plot(wl, oi_law_fit([wl, wl[1]], *oi_fit[:, mode, mode]), color=color, label="oi_law_fit")
+  # plt.plot(wl, oi_law(wl, wl[1], oi_fit[:, mode, mode]), color=color, label="oi_law")
+  plt.plot(wl, oi[:, 1, mode, mode], color=color, ls=":", label="oi")
+  plt.hlines([oi_max[mode, mode], oi_min[mode, mode]], wl[1], wl[-1], label="max")
+  plt.hlines(expression(wl[0], wl[-1], oi_fit[:, mode, mode]), wl[1], wl[-1], ls="--", label="analytical mean")
+plt.legend()
+plt.show()
+
+print("\n\nThis is oi_fit: \n", oi_fit)
+print("\n\nThis is the numerical average: \n", oi_avg)
+print("\n\nThis is the analytical average: \n", expression(wl[0], wl[-1], oi_fit[:, :, :]))
+ 
 # =========
 # PLOTTING
 # =========

scripts/space_integrals_general.py
@@ -104,7 +104,7 @@ def compare_interferent(interfering_channels = []):
     plt.legend(loc="upper left")
     plt.tight_layout()
     plt.savefig('media/Interferent_'+str(intf)+'.pdf')
-    plt.show()
+    # plt.show()
     # X0mm is supposed to be real
     error = (X0mm-X0mm_ana)/X0mm
     plt.plot(m, np.real(error))