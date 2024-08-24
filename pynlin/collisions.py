fix: definitoin of SMF DGD must contain factor 2 pi

pynlin/collisions.py
@@ -4,6 +4,7 @@ from pynlin.fiber import Fiber
 import math
 from pynlin.fiber import Fiber, SMFiber, MMFiber
 from pynlin.wdm import WDM
+from pynlin.pulses import Pulse
 
 def get_interfering_channels(): 
   pass
@@ -40,12 +41,10 @@ def get_m_values(
     `partial_collisions_start` and `partial_collisions_end` kwargs.
     """
     partial_collisions_end = partial_collisions_start
-    if isinstance(fiber, SMFiber):
-      dgd = fiber.beta2 * 2 * np.pi * (get_frequency_spacing(a_chan, b_chan, wdm))
-    elif isinstance(fiber, MMFiber):
-      dgd = (fiber.group_delay.evaluate_beta1(a_chan[0], wdm.frequency_grid()[a_chan[1]])-fiber.group_delay.evaluate_beta1(b_chan[0], b_chan[1]))
-    
-    m_max = (fiber.length * dgd) / T 
+    dgd = get_dgd(a_chan, b_chan, fiber, wdm)
+    m_max = -(fiber.length * dgd) / T
+    print(m_max)
+    print(-m_max * T /dgd)
     if m_max < 0:
         m_max = math.ceil(m_max)
         return np.arange(m_max - partial_collisions_start, partial_collisions_end + 1)
@@ -54,8 +53,41 @@ def get_m_values(
         return np.arange(-partial_collisions_start, m_max + partial_collisions_end + 1)
 
 
-def get_collision_location(m, fiber, channel_spacing, T) -> float:
+def get_collision_location(m, 
+                           fiber: Fiber, 
+                           wdm: WDM, 
+                           a_chan:Tuple[int, int], 
+                           b_chan: Tuple[int, int],
+                           pulse: Pulse) -> float:
     """For the specified index m, compute the position of the corresponding
     complete collision."""
-    return -m * T / (fiber.beta2 * 2 * math.pi * channel_spacing)
+    dgd = get_dgd(a_chan, b_chan, fiber, wdm)
+    # print(f" DGD: {dgd}, a_chan: {a_chan}, b_chan: {b_chan}")
+    return -m / (pulse.baud_rate *  dgd)
   
+  
+def get_dgd(a_chan, b_chan, fiber, wdm) -> float:
+    freq_grid = wdm.frequency_grid()
+    if isinstance(fiber, SMFiber):
+        assert (a_chan[0] == 0 and b_chan[0] == 0)
+        return fiber.beta2 * 2 * np.pi * (freq_grid[b_chan[1]] - freq_grid[a_chan[1]])
+    elif isinstance(fiber, MMFiber):
+        return fiber.group_delay.evaluate_beta1(b_chan[0], freq_grid[b_chan[1]]) - fiber.group_delay.evaluate_beta1(a_chan[0], freq_grid[a_chan[1]])
+
+
+def get_gvd(b_chan, fiber, wdm) -> float:
+    if isinstance(fiber, SMFiber):
+        return fiber.beta2
+    elif isinstance(fiber, MMFiber):
+        return fiber.group_delay.evaluate_beta2(b_chan[0], wdm.frequenc_grid()[b_chan[1]])
+    pass
+  
+  
+def get_z_walkoff(
+  fiber: Fiber, 
+  wdm: WDM, 
+  a_chan: Tuple[int, int], 
+  b_chan: Tuple[int, int], 
+  pulse: Pulse):
+  dgd = get_dgd(a_chan, b_chan, fiber, wdm)
+  return 1/ (pulse.baud_rate * dgd)

pynlin/nlin.py
@@ -1,6 +1,7 @@
 import functools
 import math
 from typing import Tuple, List
+from numba import jit
 
 import h5py
 import numpy as np
@@ -13,7 +14,7 @@ from itertools import product
 from pynlin.fiber import Fiber, SMFiber, MMFiber
 from pynlin.pulses import Pulse, RaisedCosinePulse, GaussianPulse, NyquistPulse
 from pynlin.wdm import WDM
-from pynlin.collisions import get_interfering_frequencies, get_m_values, get_interfering_channels, get_frequency_spacing
+from pynlin.collisions import get_interfering_frequencies, get_m_values, get_interfering_channels, get_frequency_spacing, get_collision_location, get_z_walkoff, get_dgd
 
 
 def apply_chromatic_dispersion(
@@ -47,26 +48,9 @@ def apply_chromatic_dispersion(
 
 
 def get_interfering_channels(a_chan: Tuple, wdm: WDM, fiber: Fiber):
-    b_chans = list(product(range(wdm.num_channels), range(fiber.n_modes)))
+    b_chans = list(product(range(fiber.n_modes), range(wdm.num_channels)))
     b_chans.remove(a_chan)
     return b_chans
-
-
-def get_dgd(a_chan, b_chan, fiber, wdm):
-    freq_grid = wdm.frequency_grid()
-    if isinstance(fiber, SMFiber):
-        assert (a_chan[1] == 0 and b_chan[1] == 0)
-        return fiber.beta2 * (freq_grid(b_chan[0]) - freq_grid(a_chan[0]))
-    elif isinstance(fiber, MMFiber):
-        return fiber.group_delay.evaluate_beta1(b_chan[0], freq_grid[b_chan[1]]) - fiber.group_delay.evaluate_beta1(a_chan[0], freq_grid[a_chan[1]])
-
-
-def get_gvd(b_chan, fiber, wdm):
-    if isinstance(fiber, SMFiber):
-        return fiber.beta2
-    elif isinstance(fiber, MMFiber):
-        return fiber.group_delay.evaluate_beta2(b_chan[0], wdm.frequenc_grid()[b_chan[1]])
-    pass
 
 
 def iterate_time_integrals(
@@ -75,34 +59,55 @@ def iterate_time_integrals(
     a_chan: Tuple,  # WDM index and mode
     pulse: Pulse,
     filename: str,
+    overwrite = False,
     **compute_collisions_kwargs,
 ) -> None:
     """Compute the inner time integral of the expression for the XPM
     coefficients Xhkm for each combination of frequencies in the supplied WDM
     grid."""
-    assert (isinstance(fiber, SMFiber) and a_chan[1] == 0)
+    assert (isinstance(fiber, SMFiber) and a_chan[0] == 0)
 
-    # open the file to save data to disk
-    with h5py.File('results/results.hdf5', 'a') as file:
-        if f"time_integrals/channel_{a_chan}/" in file:
-            print("A chan group already present on file. Nothing to do.")
-            return -1
-
+    append_write = "a"
+    found = False
+    try:
+      with h5py.File(filename, 'r') as file:
+          for gg in file["time_integrals"]:
+            print(gg)
+            
+          if f"channel_{a_chan}" in file["time_integrals"]:
+              print("A-chan group already present on file. Nothing to do.")
+              found = True
+    except FileNotFoundError:
+        print(f"File {filename} not found. Creating a new file.")
+        append_write = "w"
+    
+    if overwrite and found:
+      print("\033[91m warn: \033[0m overwriting by deleting and rewriting all the results file!")
+      append_write = "w" 
+    elif found and not overwrite:
+      return -1
+    
     print("No groups found for this A channel, calculating...")
-    file = h5py.File(filename, "a")
-
+    file = h5py.File(filename, append_write)
+    
     frequency_grid = wdm.frequency_grid()
+    a_freq = frequency_grid[a_chan[1]]
+    group_name = f"time_integrals/channel_{a_chan}/"
+    group = file.create_group(group_name)
+    group.attrs["mode"] = a_chan[0]
+    group.attrs["frequency"] = a_freq
+    
     b_channels = get_interfering_channels(
         a_chan, wdm, fiber,
     )
 
     # iterate over all channels of the WDM grid
     for b_num, b_chan in enumerate(b_channels):
-        # set up the progress bar to iterate over all interfering channels
-        # of the current channel of interest
-        interfering_frequencies_pbar = tqdm.tqdm(b_channels)
-        interfering_frequencies_pbar.set_description(
-            f"Interfering frequencies of channel {b_chan}"
+        # set up the progress bar to iterate over 
+        # all interfering channels of the current channel of interest
+        pbar = tqdm.tqdm(b_channels)
+        pbar.set_description(
+            f"A-chan: {a_chan}, B-chan = {b_chan}"
         )
 
         z, I, M = compute_all_collisions_time_integrals(
@@ -113,18 +118,11 @@ def iterate_time_integrals(
             pulse,
             **compute_collisions_kwargs,
         )
-        # saving the result
-        # each Channel of interest gets its own group with its attributes
-        a_freq = frequency_grid[a_chan[1]]
-        b_freq = frequency_grid[b_chan[1]]
-        group_name = f"time_integrals/channel_{a_chan}/"
-        group = file.create_group(group_name)
-        group.attrs["mode"] = a_chan[0]
-        group.attrs["frequency"] = a_freq
 
         # in each COI group, create a group for each interfering channel
         # and store the z-array (positions inside the fiber)
         # and the time integrals for each collision.
+        b_freq = frequency_grid[b_chan[1]]
         interferer_group_name = group_name + \
             f"interfering_channel_{b_chan}/"
         interferer_group = file.create_group(interferer_group_name)
@@ -167,59 +165,109 @@ def compute_all_collisions_time_integrals(
         Array of collision indeces
     """
     T = 1 / pulse.baud_rate
-
-    M = get_m_values(
+    f_grid = wdm.frequency_grid()
+    m_list = get_m_values(
         fiber,
         wdm,
         a_chan,
         b_chan,
         T,
         partial_collisions_margin,
+    )[::-1]
+    z_axis_list = []
+    z_walkoff = get_z_walkoff(fiber, wdm, a_chan, b_chan, pulse)
+    print("==============")
+    print(f"a: {a_chan}, b: {b_chan}, a_freq:{f_grid[a_chan[1]]:.5e}, b_freq:{f_grid[b_chan[1]]:.5e}")
+    print(f"dgd: {get_dgd(a_chan, b_chan, fiber, wdm)}, z_w: {z_walkoff}, lenght/z_w:{fiber.length/z_walkoff:.5e}")
+    print("==============")
+    # fill the z_axis_grid with good estimates of 
+    # where the pulses start and end the collision
+    # print(get_frequency_spacing(a_chan, b_chan, wdm))
+    
+    n_rough_grid = 200
+    spacing = get_frequency_spacing(a_chan, b_chan, wdm)
+    def i_func(m, z): return m_th_time_integral_Gaussian(
+      pulse,
+      fiber,
+      wdm,
+      a_chan,
+      b_chan, 
+      spacing,
+      m, 
+      z
     )
-    # first, create the Pulse object with the appropriate parameters
-    # compute the maximum delay between pulses and use it to set
-    # the number of symbols in the rcos signal
-    # tau_max = fiber.beta2 * fiber.length * 2 * np.pi * channel_spacing
-    # num_symbols = np.abs(4 * math.ceil(tau_max / T))
-
-    npoints_z = math.ceil(len(M) * points_per_collision)
-    z = np.linspace(0, fiber.length, npoints_z)
-
+    print(m_list)
+    for m in m_list:
+      # print(f"m={m}")
+      z_m = get_collision_location(m, fiber, wdm, a_chan, b_chan, pulse)
+      z_min = z_m - z_walkoff/2 * 10
+      z_max = z_m + z_walkoff/2 * 10
+      i_sample_z_m = i_func(m, z_m)
+      # print(f"m: {m}, z_m: {z_m:.5e}, z_w: {z_walkoff:.5e}")
+      # print(f"maximum at z_m: {i_sample_z_m:10.4e}")
+      # print((z_min, z_max))
+      dz = fiber.length / n_rough_grid
+      threshold = i_sample_z_m / 100
+      z = 0
+      i_sample = i_func(m, z)
+      i_sample = i_func(m, z_min)
+      # print(f"{i_sample:10.4e}")
+      while i_sample > threshold and z_min > 0:
+        z_min -= dz
+        i_sample = i_func(m, z_min)
+        # print("hit")
+      
+      i_sample = i_func(m, z_max)
+      while i_sample > threshold and z_max < fiber.length:
+        z_max += dz
+        i_sample = i_func(m, z_max)
+      n_z_points = 20
+      z_min = max(z_min, 0)
+      z_max = min(z_max, fiber.length)
+      if z_min > z_max:
+        z_axis_list.append(None)
+      else:
+        print((z_min, z_max))
+        z_axis_list.append(np.linspace(z_min, z_max, n_z_points))
+    exit()
+    
     # channel_spacing_GHz = channel_spacing * 1e-9
     # interfering_frequency_THz = interfering_frequency * 1e-12
     # coi_frequency_THz = frequency_of_interest * 1e-12
-    freq_spacing = get_frequency_spacing(a_chan, b_chan, wdm)
+    # freq_spacing = get_frequency_spacing(a_chan, b_chan, wdm)
 
-    if speedup_pulse_propagation:
-        g, t = pulse.data()
-        dt = t[1] - t[0]
-        nsamples = len(g)
-        freq = np.fft.fftfreq(nsamples, d=dt)
-        omega = 2 * np.pi * freq
-        omega = np.fft.fftshift(omega)
-        beta2 = fiber.beta2
-        gf = np.fft.fftshift(np.fft.fft(g))
-        pulse_matrix = np.zeros((len(t), len(z)), dtype=complex)
-        for i, L in enumerate(z):
-            propagator = -1j * beta2 / 2 * omega**2 * L
-            gf_propagated_1 = gf * np.exp(propagator)
-            pulse_matrix[:, i] = np.fft.ifft(np.fft.fftshift(gf_propagated_1))
-    pbar_description = (
-        f"Collisions between ({a_chan}"
-        + f" and {b_chan}) THz, spacing {freq_spacing} GHz"
-    )
+    # if speedup_pulse_propagation:
+    #     g, t = pulse.data()
+    #     dt = t[1] - t[0]
+    #     nsamples = len(g)
+    #     freq = np.fft.fftfreq(nsamples, d=dt)
+    #     omega = 2 * np.pi * freq
+    #     omega = np.fft.fftshift(omega)
+    #     beta2 = fiber.beta2
+    #     gf = np.fft.fftshift(np.fft.fft(g))
+    #     pulse_matrix = np.zeros((len(t), len(z)), dtype=complex)
+    #     for i, L in enumerate(z):
+    #         propagator = -1j * beta2 / 2 * omega**2 * L
+    #         gf_propagated_1 = gf * np.exp(propagator)
+    #         pulse_matrix[:, i] = np.fft.ifft(np.fft.fftshift(gf_propagated_1))
+    
+    # pbar_description = (
+    #     f"Collisions between ({a_chan}"
+    #     + f" and {b_chan}) THz, spacing {freq_spacing} GHz"
+    # )
 
     if use_multiprocessing:
-        # build a partial function otherwise multiprocessing complains about
-        # not being able to pickle stuff
+        # build a partial function otherwise multiprocessing
+        # complains about not being able to pickle stuff
         if speedup_pulse_propagation:
             partial_function = functools.partial(
-                X0mm_time_integral_multiprocessing_wrapper, pulse, fiber, wdm, a_chan, b_chan, z
+                m_th_time_integral, pulse, fiber, wdm, a_chan, b_chan,
             )
-            time_integrals = process_map(
-                partial_function, M, leave=False, desc=pbar_description, chunksize=1
+            integrals_list = process_map(
+                partial_function, m_list, z_axis_list, leave=False, chunksize=1
             )
         else:
+            raise NotImplementedError("No pulse manual propagation")
             partial_function = functools.partial(
                 X0mm_time_integral_precomputed_multiprocessing_wrapper, pulse_matrix, fiber, z, t, channel_spacing, pulse.T0
             )
@@ -227,6 +275,7 @@ def compute_all_collisions_time_integrals(
                 partial_function, M, leave=False, desc=pbar_description, chunksize=1
             )
     else:
+        raise NotImplementedError("No single-processing")
         collisions_pbar = tqdm.tqdm(M, leave=False)
         collisions_pbar.set_description(pbar_description)
         time_integrals = []
@@ -250,12 +299,13 @@ def compute_all_collisions_time_integrals(
             time_integrals.append(I)
 
     # convert the list of arrays in a 2d array, since the shape is the same
-    I = np.stack(time_integrals)
-    return z, time_integrals, M
+    z_axis_list_2d = np.stack(z_axis_list)
+    integrals_list_2d = np.stack(integrals_list)
+    return z_axis_list_2d, integrals_list_2d, m_list
 
 
 # Multiprocessing wrapper
-def X0mm_time_integral_multiprocessing_wrapper(
+def m_th_time_integral(
     pulse: Pulse,
     fiber: Fiber,
     wdm: WDM,
@@ -268,9 +318,46 @@ def X0mm_time_integral_multiprocessing_wrapper(
     if isinstance(pulse, NyquistPulse):
         raise NotImplementedError("no Nyquist Pulse yet")
     elif isinstance(pulse, GaussianPulse):
-        return X0mm_time_integral_Gaussian(pulse, fiber, wdm, a_chan, b_chan, freq_spacing, m, z)
+        return m_th_time_integral_Gaussian(pulse, fiber, wdm, a_chan, b_chan, freq_spacing, m, z)
     else:
         raise NotImplementedError("no generic Pulse yet")
+
+# @jit
+def m_th_time_integral_Gaussian(
+    pulse: Pulse,
+    fiber: Fiber,
+    wdm: WDM,
+    a_chan: Tuple[int, int],
+    b_chan: Tuple[int, int],
+    freq_spacing: float,
+    m: int,
+    z: float
+) -> float:
+    # Apply the fully analytical formula
+    if isinstance(fiber, SMFiber):
+        l_d = 1 / (np.abs(fiber.beta2) * (pulse.baud_rate)**2)
+        dgd = fiber.beta2 * 2 * np.pi * (freq_spacing)
+        factor1 = pulse.baud_rate / (np.sqrt(2 * np.pi))
+        factor2 = 1 / np.sqrt(1 + (z / l_d)**2)
+        # print(f"=== z: {z:.3e}, exponent: {m + pulse.baud_rate * dgd * z:.3e}")
+        exponent = -((m + pulse.baud_rate * dgd * z)** 2) / (2 * (1 + (z / l_d)**2))
+        # print(f"m/pulse.baud_rate : {m:.5e}, dgd z: {pulse.baud_rate * dgd * z:.5e}, exponent : {exponent:.5e}")
+        return factor1 * factor2 * np.exp(exponent)
+    if isinstance(fiber, MMFiber):
+        l_da = 1 / \
+            (fiber.group_delay.evaluate_beta2(
+                a_chan[0], wdm.frequency_grid()[a_chan[1]])(pulse.baud_rate)**2)
+        l_db = 1 / \
+            (fiber.group_delay.evaluate_beta2(
+                b_chan[0], wdm.frequency_grid()[b_chan[1]])(pulse.baud_rate)**2)
+        dgd = fiber.group_delay.evaluate_beta1(b_chan[0], wdm.frequency_grid(
+        )[b_chan[1]]) - fiber.group_delay.evaluate_beta1(a_chan[0], wdm.frequency_grid()[a_chan[1]])
+        avg_l_d = (l_da * l_db) / (l_da + l_db) / 2
+        factor1 = pulse.baud_rate / (np.sqrt(2 * np.pi))
+        factor2 = 1 / np.sqrt(1 + (z / avg_l_d)**2)
+        exponent = -((m/pulse.baud_rate + dgd * z)**2) / (2 * (1 + (z / avg_l_d)**2))
+        return factor1 * factor2 * np.exp(exponent)
+    pass
 
 
 # def X0mm_time_integral_multiprocessing_wrapper(
@@ -287,45 +374,10 @@ def X0mm_time_integral_multiprocessing_wrapper(
 #     """A wrapper for the `X0mm_time_integral` function to easily
 #     `functool.partial` and enable multiprocessing."""
 #     return X0mm_time_integral_precomputed(pulse_matrix, fiber, z, t, channel_spacing, m, T)
-
-
 # Numerical or semianalytical methods for time integrals
-def X0mm_time_integral_Gaussian(
-    pulse: Pulse,
-    fiber: Fiber,
-    wdm: WDM,
-    a_chan: Tuple[int, int],
-    b_chan: Tuple[int, int],
-    freq_spacing: float,
-    m: int,
-    z: float
-) -> np.ndarray:
-    # Apply the fully analytical formula
-    if isinstance(fiber, SMFiber):
-        l_d = 1 / (np.abs(fiber.beta2) * (pulse.baud_rate)**2)
-        factor1 = pulse.baud_rate / (np.sqrt(2 * np.pi))
-        factor2 = 1 / np.sqrt(1 + (z / l_d)**2)
-        exponent = -((m + fiber.beta2 * (2 * np.pi * freq_spacing) * z)
-                     ** 2) / (2 * (1 + (z / l_d)**2))
-        return factor1 * factor2 * np.exp(exponent)
-    if isinstance(fiber, MMFiber):
-        l_da = 1 / \
-            (fiber.group_delay.evaluate_beta2(
-                a_chan[0], wdm.frequency_grid()[a_chan[1]])(pulse.baud_rate)**2)
-        l_db = 1 / \
-            (fiber.group_delay.evaluate_beta2(
-                b_chan[0], wdm.frequency_grid()[b_chan[1]])(pulse.baud_rate)**2)
-        dgd = fiber.group_delay.evaluate_beta1(b_chan[0], wdm.frequency_grid(
-        )[b_chan[1]]) - fiber.group_delay.evaluate_beta1(a_chan[0], wdm.frequency_grid()[a_chan[1]])
-        avg_l_d = (l_da * l_db) / (l_da + l_db) / 2
-        factor1 = pulse.baud_rate / (np.sqrt(2 * np.pi))
-        factor2 = 1 / np.sqrt(1 + (z / avg_l_d)**2)
-        exponent = -((m + dgd * z)**2) / (2 * (1 + (z / avg_l_d)**2))
-        return factor1 * factor2 * np.exp(exponent)
-    pass
 
 
-def X0mm_time_integral_Nyquist(
+def m_th_time_integral_Nyquist(
     pulse: Pulse,
     fiber: Fiber,
     z: np.ndarray,