fix: beta1 loading into MMFiber from Matlab results scaling

scripts/modules/beta_load.py
@@ -8,14 +8,16 @@ import seaborn as sns
 import pynlin.wdm
 from pynlin.utils import nu2lambda
 from scripts.modules.load_fiber_values import load_group_delay
+from numpy import polyval
+from pynlin.fiber import MMFiber
 
 import json
 rc('text', usetex=True)
 logging.basicConfig(filename='MMF_optimizer.log', encoding='utf-8', level=logging.INFO)
 log = logging.getLogger(__name__)
-log.debug("starting to load sim_config.json")
+log.debug("starting to load s+c+l_config.json")
 
-f = open("./scripts/sim_config.json")
+f = open("./scripts/s+c+l_config.json")
 data = json.load(f)
 # print(data)
 dispersion = data["dispersion"]
@@ -46,7 +48,7 @@ oi_avg = np.load('oi_avg.npy')
 use_avg_oi = False
 
 beta1_params = load_group_delay()
-print(beta1_params.shape)
+# print(beta1_params.shape)
 
 wdm = pynlin.wdm.WDM(
     spacing=channel_spacing,
@@ -57,9 +59,16 @@ freqs = wdm.frequency_grid()
 modes = [0, 1, 2, 3]
 mode_names = ['LP01', 'LP11', 'LP21', 'LP02']
 
+fiber = MMFiber(
+    effective_area=80e-12,
+    overlap_integrals = oi_fit,
+    group_delay = beta1_params,
+    length=100e3
+)
+
 beta1 = np.zeros((len(modes), len(freqs)))
 for i in modes:
-  beta1[i, :] = beta1_polynomial_expansion(beta1_params, i, freqs)
+  beta1[i, :] = fiber.group_delay.evaluate_beta1(i, freqs)
 
 plt.clf()
 sns.heatmap(beta1, cmap="coolwarm", square=False, xticklabels=freqs, yticklabels=modes)

scripts/modules/load_fiber_values.py
@@ -11,14 +11,15 @@ def convert_coefficients(p1, p2, p3):
     avg = scipy.io.loadmat(beta_file)['omega_mean'][0][0]
     p1_new = 3 * p1 / (std**3)
     p2_new = 2 * p2 / (std**2) - 6 * p1 * avg / (std**3)
-    p3_new = 3 * (avg**2) / (std**3) * p1 - 2 * p2 * avg / (std**2) + p3
-    return [p1_new, p2_new, p3_new]
+    p3_new = 3 * (avg**2) / (std**3) * p1 - 2 * p2 * avg / (std**2) + p3 / std
+    return [p1_new * (2 * np.pi)**2, p2_new * 2 * np.pi, p3_new]
 
 
 def load_group_delay() -> np.array:
     beta_file = './results/fitBeta.mat'
     mat = scipy.io.loadmat(beta_file)['fitParams'] * 1.0
     for i in range(4):
+      print(mat[i, 0], mat[i, 1], mat[i, 2])
       print(convert_coefficients(mat[i, 0], mat[i, 1], mat[i, 2]))
       mat[i, :] = convert_coefficients(mat[i, 0], mat[i, 1], mat[i, 2])
     return mat